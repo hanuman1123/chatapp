2b3638a3d0b8ca8cd34f85438c7e61b920f1b620
hanuman1123 <jagadeeshhanumantadi@gmail.com>
2025-07-24 07:30:34 -0400

    Fix __dirname issue for production deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,11 +10,16 @@ import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import { app, server } from "./lib/socket.js";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
-const __dirname = path.resolve();
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 app.use(express.json());
 app.use(cookieParser());
@@ -29,10 +34,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "/frontend/dist/index.html"));
   });
 }
 
