e512e0933ac5d5a7d63a647f18458a5903f3ceca
hanuman1123 <jagadeeshhanumantadi@gmail.com>
2025-07-24 07:02:24 -0400

    fixed all the bugs

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,19 +2,16 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+
 import path from "path";
-import { createServer } from "http";
 
 import { connectDB } from "./lib/db.js";
+
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
-import { initSocket } from "./lib/socket.js";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
-
-const app = express();
-const server = createServer(app); // use same app
-initSocket(server); // pass server to socket.io setup
 
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
@@ -33,12 +30,13 @@ app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
 server.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+  console.log("server is running on PORT:" + PORT);
   connectDB();
 });
